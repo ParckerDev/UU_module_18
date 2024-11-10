11f03666caebdd6d1b431c1de3270d719834e2c3
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-10 19:41:02 +0300

    add apps in settings file

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'example1',
+    'example2',
+    'example3',
+    'task2',
 ]
 
 MIDDLEWARE = [
