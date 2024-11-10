b7b105feb0bc7d3dd7d204fcaf8a3dd2aba97b2c
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-10 20:23:01 +0300

    add funcview and classview

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'UrbanDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates/secondtask'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
