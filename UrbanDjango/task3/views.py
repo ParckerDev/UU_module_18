7ff47201b6d5c7119dfc52243413234f6155f534
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-11 17:51:03 +0600

    Добавил изображения, подключил папку со статикой в проект. Закончил таску.

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -4,10 +4,25 @@ from django.shortcuts import render
 TMP_FLDR = 'third_task/'
 
 def main_page(request):
-    return render(request, TMP_FLDR+'main_page.html')
+    title = 'Главнвя страница'
+    text = 'В компании ParkerBrewery мы стремимся создавать уникальные сорта пива, которые радуют ценителей и новичков. Наша команда опытных пивоваров использует только лучшие ингредиенты: от отборного ячменя до свежих хмелей, чтобы обеспечить высокое качество и насыщенный вкус каждого напитка. Мы гордимся традициями пивоварения, сочетая их с инновационными методами, что позволяет нам разрабатывать оригинальные рецепты. В нашем ассортименте вы найдете как классические сорта, так и крафтовые новинки, каждая из которых рассказывает свою историю. ParkerBrewery — это не просто пиво, это искусство, созданное с любовью и страстью.'
+
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, TMP_FLDR+'main_page.html', context)
 
 def second_page(request):
-    return render(request, TMP_FLDR+'second_page.html')
+    title = 'Купить BEER'
+    context = {
+        'title': title
+    }
+    return render(request, TMP_FLDR+'second_page.html', context)
 
 def third_page(request):
-    return render(request, TMP_FLDR+'third_page.html')
+    title = 'Корзина'
+    context = {
+        'title': title
+    }
+    return render(request, TMP_FLDR+'third_page.html', context)
